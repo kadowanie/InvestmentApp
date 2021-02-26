144407d91832a148bb3a157c04070bcd29120ea8
kadowanie <kadowanie@gmail.com>
2021-02-26 21:11:34 +0300

    add main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double percent = 3;
+        int investSum = 1000;
+        double income = investSum * (percent / 100);
+        System.out.println(income);
+    }
+}
