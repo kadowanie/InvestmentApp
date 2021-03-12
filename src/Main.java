d56865885b0cf04c56a379bc94a8d983bd92ffa0
kadowanie <kadowanie@gmail.com>
2021-03-12 17:53:05 +0300

    issue fix - income data type correction

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double percent = 3;
+        float percent = 3;
         int investSum = 1000;
-        double income = investSum * (percent / 100);
-        System.out.println(income);
+        int income = (int) (investSum * 100 * (percent / 100));
+        System.out.println(income / 100);
     }
 }
